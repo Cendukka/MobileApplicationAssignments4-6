414d94af3444321ef6dfc08c4be95cf28433b97e
Samuli LEhtonen <samuilehtonen33@gmail.com>
2020-03-05 17:03:13 -0500

    Assignment 4: Task 2
    Did the task 2 from asignment 4

--- JSONSimpleExample/app/src/main/java/com/example/jsonsimpleexample/MainActivity.java
+++ JSONSimpleExample/app/src/main/java/com/example/jsonsimpleexample/MainActivity.java
@@ -10,10 +10,11 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
 
-    String JSON_STRING = "{\"employee\":{\"name\":\"Joe Mathew\",\"salary\":55000}}";
+    String JSON_STRING = "{\"employee\":{\"name\":\"Joe Mathew\",\"salary\":55000, \"title\":\"Professor\"}}";
     String name;
     int salary;
-    TextView employeeName, employeeSalary;
+    String title;
+    TextView employeeName, employeeSalary, employeeTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +23,21 @@ public class MainActivity extends AppCompatActivity {
         // get the reference of TextView's
         employeeName = (TextView) findViewById(R.id.name);
         employeeSalary = (TextView) findViewById(R.id.salary);
+        employeeTitle = (TextView) findViewById(R.id.title);
 
         try {
             // get JSONObject from JSON file
             JSONObject obj = new JSONObject(JSON_STRING);
             // fetch JSONObject named employee
             JSONObject employee = obj.getJSONObject("employee");
-            // get employee name and salary
+            // get employee name, salary and title
             name = employee.getString("name");
             salary = employee.getInt("salary");
-            // set employee name and salary in TextView's
+            title = employee.getString("title");
+            // set employee name, salary and title in TextView's
             employeeName.setText("Name: " + name);
             employeeSalary.setText("Salary: " + salary);
+            employeeTitle.setText("Title: " + title);
 
         } catch (JSONException e) {
             e.printStackTrace();
