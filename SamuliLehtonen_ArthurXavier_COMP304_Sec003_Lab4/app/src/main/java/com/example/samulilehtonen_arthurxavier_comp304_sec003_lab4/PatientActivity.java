ead9682480aeda5cc49f692db7db0c2501a94d11
Samuli LEhtonen <samuilehtonen33@gmail.com>
2020-03-10 12:03:23 -0400

    Initialized main activity with buttons

---
+++ SamuliLehtonen_ArthurXavier_COMP304_Sec003_Lab4/app/src/main/java/com/example/samulilehtonen_arthurxavier_comp304_sec003_lab4/PatientActivity.java
@@ -0,0 +1,14 @@
+package com.example.samulilehtonen_arthurxavier_comp304_sec003_lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient);
+    }
+}
