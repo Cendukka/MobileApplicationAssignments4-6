09b763e3a66c5743d039afb05f98df1844459d4f
Samuli Lehtonen <samulilehtonen33@gmail.com>
2020-04-07 16:09:08 +0300

    Lab6: Sending SMS
    The app sends sms to it's own phone number now, and taking the receiver name

--- SamuliLehtonen_ArthurXavier_COMP304_Sec003_Lab6/app/src/main/java/com/example/samulilehtonen_arthurxavier_comp304_sec003_lab6/InfoActivity.java
+++ SamuliLehtonen_ArthurXavier_COMP304_Sec003_Lab6/app/src/main/java/com/example/samulilehtonen_arthurxavier_comp304_sec003_lab6/InfoActivity.java
@@ -2,14 +2,26 @@ package com.example.samulilehtonen_arthurxavier_comp304_sec003_lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class InfoActivity extends AppCompatActivity {
 
     EditText customerName, customerPhone, customerEmail;
     TextView productsResume;
+    ImageButton submit;
+    IntentFilter intentFilter;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,48 @@ public class InfoActivity extends AppCompatActivity {
         customerPhone = (EditText) findViewById(R.id.txtPhone);
         customerEmail = (EditText) findViewById(R.id.txtEmail);
         productsResume = (TextView) findViewById(R.id.txtProductResume);
+        submit = (ImageButton) findViewById(R.id.btnFinish);
+
+        context = getApplicationContext();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("SMS_RECEIVED_ACTION");
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String custName = customerName.getText().toString();
+                String cutPhone = customerPhone.getText().toString();
+                String custEmail = customerEmail.getText().toString();
+
+                if(!custName.equals("") && !cutPhone.equals("")){
+                    String custNameEdit = custName.substring(0, 1).toUpperCase() + custName.substring(1).toLowerCase();
+                    String submitedSMS = "Thank you for your purchase, "+custNameEdit+" ! We will inform you when we have sent your ordered items!";
+                    //String submitedSMS = custName;
+                    String submitedEMAIL = "Thank you for your purchase! We will inform you when we have sent your ordered items!";
+
+                    try{
+                        sendMsg(submitedSMS, "+15555215554");
+                    }catch (Exception e){
+                        Toast.makeText(context, "Something went wrong while sending SMS. Check permissions and Try again.", Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(context, "Fill all fields", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+        });
 
     }
+
+    void sendMsg(String smsMessage, String smsNumber){
+        PendingIntent sentPI = PendingIntent.getBroadcast(this,0, new Intent("Sent"), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this,0, new Intent("Delivered"), 0);
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(smsNumber,null, smsMessage, sentPI, deliveredPI);
+    }
+
 }
