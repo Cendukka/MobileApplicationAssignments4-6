d84511540c0b5f039688649ae15b270894c1eb2c
Samuli Lehtonen <samulilehtonen33@gmail.com>
2020-04-10 21:36:00 +0300

    request sms permission

--- SamuliLehtonen_ArthurXavier_COMP304_Sec003_Lab6/app/src/main/java/com/example/samulilehtonen_arthurxavier_comp304_sec003_lab6/MainActivity.java
+++ SamuliLehtonen_ArthurXavier_COMP304_Sec003_Lab6/app/src/main/java/com/example/samulilehtonen_arthurxavier_comp304_sec003_lab6/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.samulilehtonen_arthurxavier_comp304_sec003_lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button productsButton;
+    private int SMS_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         productsButton = (Button)findViewById(R.id.btnBuy);
 
+
+
         productsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +33,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        SMS_PERMISSION);
+        }
     }
 }
